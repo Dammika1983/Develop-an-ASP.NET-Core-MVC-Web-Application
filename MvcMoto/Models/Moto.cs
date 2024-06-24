0c01df5b4bc7b725880b9438eb3bb9c30f384958
OWNER <OWNER@DAMMIKA>
2024-06-23 22:13:59 -0400

    Add project files.

---
+++ MvcMoto/Models/Moto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MvcMoto.Models
+{
+    public class Moto
+    {
+        public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+
+        public string? Name { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+
+        public string? Circuit { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+
+        public String? Team { get; set; }
+        [Required]
+        [Display(Name = "World Rank")]
+        public int WorldRank { get; set; }
+        [Required]
+        [Display(Name = "Driver Number")]
+        public int DriverNumber { get; set; }
+
+    }
+}
---
+++ MvcMoto/Models/SeedData.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMoto.Data;
+using System;
+using System.Linq;
+
+namespace MvcMoto.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMotoContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMotoContext>>()))
+            {
+                // Look for any motoes.
+                if (context.Moto.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.Moto.AddRange(
+                    new Moto
+                    {
+                        Name = "Sergio Perez",
+                        Circuit = "F1",
+                        Team = "Red Bull Racing",
+                        WorldRank = 2,
+                        DriverNumber = 11
+                    },
+                    new Moto
+                    {
+                        Name = "Alex Bowman",
+                        Circuit = "NASCAR",
+                        Team = "Hendrick Motorsports",
+                        WorldRank = 2,
+                        DriverNumber = 48
+                    },
+                    new Moto
+                    {
+                        Name = "Ralph Boschung",
+                        Circuit = "FIA F2",
+                        Team = "Campos Racing",
+                        WorldRank = 1,
+                        DriverNumber = 25
+                    },
+                    new Moto
+                    {
+                        Name = "Oliver Goeth",
+                        Circuit = "FIA F3",
+                        Team = "Trident",
+                        WorldRank = 2,
+                        DriverNumber = 5
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
